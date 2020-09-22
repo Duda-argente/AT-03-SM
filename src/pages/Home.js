1f8c967ee759a677aa14e79f0d1656a8257a1936
Duda-argente <argenteeduarda@gmail.com>
2020-09-22 20:07:15 -0300

    Alterei

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,29 +2,46 @@ import React from 'react';
 import {StyleSheet, View , Text , TextInput, Button} from 'react-native';
 import IconEntypo from 'react-native-vector-icons/Entypo';
 import { StatusBar } from 'expo-status-bar';
+import {Picker} from '@react-native-community/picker';
 
 IconEntypo.loadFont();
 
 
+
 export function Home() {
+  const[linguagem, setlinguagem] = React.useState('0')
   return (
     <View style={styles.container}>
       <View style ={styles.duda}><StatusBar style="auto"/>
       <Text style={styles.dudinha}>ACADEMIA 4137 FITNESS</Text>
      
-      <Text style={styles.lucifer}> <IconEntypo name="tv" size={25} color="#C71585"></IconEntypo>AQUI VOCÊ VAI PODER CALCULAR O SEU ÍNDICE DE MASSA CALCULAR E VER O SEU PERFIL{"\n"}
+      <Text style={styles.lucifer}> <IconEntypo name="heart" size={40} color="#C71585"></IconEntypo>AQUI VOCÊ VAI PODER CALCULAR O SEU ÍNDICE DE MASSA CALCULAR E VER O SEU PERFIL{"\n"}
       
-      ENTÃO, VENHA PARA O APLICATIVO DA MELHOR ACADEMIA DA SUA REGIÃO.</Text>
+      ENTÃO, VENHA PARA O APLICATIVO DA MELHOR ACADEMIA DA SUA REGIÃO.  <IconEntypo name="heart" size={40} color="#C71585"></IconEntypo></Text>
       </View>
 
 
 
 
       <View style={styles.resto}>
-        <Text style={styles.restoS}>Digite as informações solicitadas</Text>
+        <Text style={styles.restoS}><IconEntypo name="heart" size={40} color="#C71585"></IconEntypo>Digite as informações solicitadas</Text>
         <TextInput style={styles.input} onChangeText={text => onChangeText("NADA")} value={"\tPeso"}></TextInput>
         <TextInput style={styles.flor} onChangeText={text => onChangeText("NADA")} value={"\tAltura"}></TextInput>
-        <Button  style={{marginTop:100}} title ="Aperte para calcular o seu IMC" color="#F08080"></Button>
+
+        <Picker
+            selectedValue={linguagem}
+            style={{height: 50, width: 402, borderColor:"#FF69B4", backgroundColor:"#dbdbdb", fontStyle:"italic", borderWidth: 1, marginVertical:15, fontSize:20, color:"#8f8f8f",}}
+            onValueChange={(itemValue, itemIndex) =>
+          setlinguagem({linguagem: itemValue})
+           }>
+              <Picker.Item label="Escolha um item" value="0" />
+             <Picker.Item label="Criança" value="java" />
+             <Picker.Item label="Adulto" value="js" />
+             <Picker.Item label="Idoso" value="js" />
+          </Picker>
+
+          <IconEntypo name="heart" size={25} color="#C71585"></IconEntypo>
+        <Button style={{marginTop:100 , height: 50, width: 402 }} title ="Aperte para calcular o seu IMC" color="#F08080" >/</Button>
 
       </View>
       </View>
@@ -40,7 +57,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
     borderRadius: 10,
-    width: '30%',
+    width: '50%',
     height: '70%',
     alignSelf: 'center',
     marginTop: 100,
@@ -79,7 +96,7 @@ const styles = StyleSheet.create({
 
   input: {
     height: 50,
-    width: 302,
+    width: 402,
     borderColor: 'gray', 
     borderWidth: 1,
     marginVertical:15,
@@ -92,7 +109,7 @@ const styles = StyleSheet.create({
 
   flor: {
     height: 50,
-    width: 302,
+    width: 402,
     borderColor: 'gray', 
     borderWidth: 1,
     marginVertical:15,
